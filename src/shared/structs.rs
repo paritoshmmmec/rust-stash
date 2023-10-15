ecc05c0a6eee789c1ebc1ccd0fde0f1802edc5b1
Paritosh <paritoshmmmec@gmail.com>
2023-10-15 10:05:02 -0400

    mutable rust

--- src/shared/structs.rs
+++ src/shared/structs.rs
@@ -1,16 +1,28 @@
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Customer {
     first_name: String,
     last_name: String,
+    gender: Gender,
+    prefix: String,
+}
+
+#[derive(Debug, Default)]
+enum Gender {
+    #[default]
+    Male,
+    Female,
 }
 
 impl Customer {
     fn get_full_name(&self) -> String {
-        return format!("{0} {1}", &self.first_name, &self.last_name);
+        return format!("{} {} {}", &self.prefix, &self.first_name, &self.last_name);
     }
 
-    fn set_full_name(&mut self) {
-        self.first_name = String::from("Mr. ");
+    fn set_prefix(&mut self) {
+        match self.gender {
+            Gender::Male => self.prefix = String::from("Mr."),
+            Gender::Female => self.prefix = String::from("Ms."),
+        }
     }
 }
 
@@ -18,11 +30,15 @@ pub fn sub_main() {
     let first_customer = Customer {
         first_name: String::from("Paritosh"),
         last_name: String::from("Baghel"),
+        gender: Gender::Male,
+        ..Default::default()
     };
 
     let second_customer = Customer {
         first_name: String::from("Binny"),
         last_name: String::from("Rao"),
+        gender: Gender::Female,
+        ..Default::default()
     };
 
     let mut customers = Vec::new();
@@ -31,8 +47,24 @@ pub fn sub_main() {
 
     println!("=============== Printing customer information ================");
 
+    // This is how you loop through customer mutable ref
     for customer in &mut customers {
-        customer.set_full_name();
+        customer.set_prefix();
+        println!(" {}", customer.get_full_name());
+    }
+
+    // This is how you loop through customer mutable ref exactly same as above as `iter_mut`
+    for customer in customers.iter_mut() {
+        customer.set_prefix();
+        println!(" {}", customer.get_full_name());
+    }
+
+    println!("==============================================================");
+
+    println!("=============== Printing customer information ================");
+
+    // This is how you loop through customer non mutable
+    for customer in customers.iter() {
         println!(" {}", customer.get_full_name());
     }
 
